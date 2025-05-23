547edc4e920ca551efca5a360a4d82b94dd88ec1
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-23 16:50:51 -0300

    [feat] - Implements youtube component

---
+++ src/components/atoms/youtube/youtube.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type YoutubeProps = {
+  url: string;
+};
+
+export const Youtube: React.FC<YoutubeProps> = ({ url }) => {
+  if (!url) return null;
+
+  return (
+    <div className="w-full aspect-video max-h-[80vh] rounded overflow-hidden">
+      <iframe
+        src={url}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="w-full h-full"
+      />
+    </div>
+  );
+};
