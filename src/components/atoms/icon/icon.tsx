57a9e40ea628ad29b94361ac8a50523cd9a01491
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-23 02:25:54 -0300

    [feat] - Implements icon component

---
+++ src/components/atoms/icon/icon.tsx
@@ -0,0 +1,44 @@
+import LtiChevronDown from '@/assets/icons/chevron_down.svg';
+import LtiChevronLeft from '@/assets/icons/chevron_left.svg';
+import LtiChevronRight from '@/assets/icons/chevron_right.svg';
+import LtiChevronUp from '@/assets/icons/chevron_up.svg';
+import LtiFilter from '@/assets/icons/filter.svg';
+import LtiMoon from '@/assets/icons/moon.svg';
+import LtiSearch from '@/assets/icons/search.svg';
+import LtiSun from '@/assets/icons/sun.svg';
+
+type IconName =
+  | 'chevron-down'
+  | 'chevron-left'
+  | 'chevron-right'
+  | 'chevron-up'
+  | 'filter'
+  | 'moon'
+  | 'search'
+  | 'sun';
+
+type IconProps = {
+  name: IconName;
+  size?: number;
+  color?: string;
+  className?: string;
+};
+
+const iconsMap: Record<
+  IconName,
+  React.FC<{ size?: number; color?: string; className?: string }>
+> = {
+  'chevron-down': LtiChevronDown,
+  'chevron-left': LtiChevronLeft,
+  'chevron-right': LtiChevronRight,
+  'chevron-up': LtiChevronUp,
+  filter: LtiFilter,
+  moon: LtiMoon,
+  search: LtiSearch,
+  sun: LtiSun,
+};
+
+export const Icon: React.FC<IconProps> = ({ name, className }) => {
+  const Component = iconsMap[name];
+  return <Component className={className} />;
+};
