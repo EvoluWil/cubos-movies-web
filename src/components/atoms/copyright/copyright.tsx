ec3b340e3b6af869ef33c947eef492ba716a858f
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-23 15:48:43 -0300

    [feat] - Implements copyright

---
+++ src/components/atoms/copyright/copyright.tsx
@@ -0,0 +1,8 @@
+export const Copyright = () => {
+  return (
+    <p className="text-base text-gray-500 dark:text-gray-400">
+      2025 &copy; Todos os direitos reservados a{' '}
+      <span className="font-semibold">Cubos Movies</span>.
+    </p>
+  );
+};
