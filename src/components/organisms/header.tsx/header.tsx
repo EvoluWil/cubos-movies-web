4cdd2d3abee581e71beded8ee79056452111b6b1
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-26 17:25:29 -0300

    [fix] header

--- src/components/organisms/header.tsx/header.tsx
+++ src/components/organisms/header.tsx/header.tsx
@@ -1,19 +1,19 @@
-import { getUserSession } from '@/app/utils/session';
 import { Logo } from '@/components/atoms/logo/logo';
-import { Button } from '@/components/molecules/button/button';
+import { SignOutButton } from '@/components/atoms/sign-out-button/sign-out-button';
 import { ToggleTheme } from '@/components/molecules/toggle-theme/toggle-theme';
+import { getUserSession } from '@/utils/session';
 
 export const Header = async () => {
   const user = await getUserSession();
 
   return (
-    <header className="sticky top-0 left-0 w-full h-18 flex items-center border-b border-mauve_alpha-400">
+    <header className="fixed top-0 left-0 w-full h-18 z-20 flex items-center border-b border-mauve_alpha-400 backdrop-blur-[8px]">
       <div className="container flex items-center justify-between">
         <Logo />
 
         <div className="flex items-center gap-2">
           <ToggleTheme />
-          {!!user && <Button>Logout</Button>}
+          {!!user && <SignOutButton />}
         </div>
       </div>
     </header>
